0472399544061b3b15a5b8030376933fb3475fdf
IT22074072 <it22074072@my.sliit.lk>
2025-01-13 17:08:28 +0530

    connecting spring to react

--- src/main/java/org/example/springbootrest/JobRestController.java
+++ src/main/java/org/example/springbootrest/JobRestController.java
@@ -4,6 +4,7 @@ import org.example.springbootrest.model.JobPost;
 import org.example.springbootrest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     @Autowired
